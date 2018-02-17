530c6f609030c350845d8edf234ec5c1adab8ad4
Julia Nara <julianara888@gmail.com>
2018-02-17 17:13:10 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,11 @@
+require_relative 'concerns/slugifiable'
+
 class Song < ActiveRecord::Base
-	has_many :song_genres
-	belongs_to :artist
-	has_many :genres, through: :song_genres
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
+  
+  belongs_to :artist
+  has_many :song_genres
+  has_many :genres, :through => :song_genres
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,29 @@
-# Add seed data here. Seed your database with `rake db:seed`
+# asap = Artist.new(name: 'A$AP Rocky')
+# pixies = Artist.new(name: 'Pixies')
+# stooges = Artist.new(name: 'The Stooges')
+# sza = Artist.new(name: 'SZA')
 
+rap = Genre.create(name: 'rap')
+alt = Genre.create(name: 'alt')
+punk = Genre.create(name: 'punk')
+rnb = Genre.create(name: 'r&b')
+
+# peso = Song.create(name: 'Peso')
+# peso.artist = asap
+# peso.genre = rap 
+# peso.save
+
+# debaser = Song.create(name: 'Debaser')
+# debaser.artist = pixies 
+# debaser.genre = alt 
+# debaser.save 
+
+# dog = Song.create(name: 'I Wanna Be Your Dog')
+# dog.artist = stooges 
+# dog.genre = punk 
+# dog.save 
+
+# wknd = Song.create(name: 'The Weekend')
+# wknd.artist = sza 
+# wknd.genre = rnb 
+# wknd.save 
